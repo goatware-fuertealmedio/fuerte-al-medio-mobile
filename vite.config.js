bf6c20d913e84a6f6e2999cc6a0d15114418566f
EXC79707 <gespina@cfotechlatam.com>
2025-04-04 21:09:20 -0300

    build: deploy github actions

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/fuerte-al-medio-mobile/', // 👈 importante para que funcione el deploy en GitHub Pages
   plugins: [react()],
 })
